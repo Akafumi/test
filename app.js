65bbde8afd8c83ec06cb6d0ebb0178ba18c8508f
Akafumi <a.takaka513@gmail.com>
2021-07-11 06:04:09 +0900

    Create a base

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-const
+//let A = 3
+//console.log(A+A)
+
+//button[0].addEventListener('click', function{
+//window.alert('seikai');
+//})
+
+//function copyToClipboard() {
+//let copyTarget = document.getElementById("copyTarget");
+//copyTarget.select();
+//document.execCommand("copy");
+//alert("complete" + copyTarget.value);
+//}
